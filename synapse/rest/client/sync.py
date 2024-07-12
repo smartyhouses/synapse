cb2f29cb9114192e2a8a8d6ae3d4c608c7a5c132
Erik Johnston <erik@matrix.org>
2024-07-12 13:02:59 +0100

    Fixup

--- synapse/rest/client/sync.py
+++ synapse/rest/client/sync.py
@@ -988,7 +988,7 @@ class SlidingSyncRestServlet(RestServlet):
         serialized_rooms: Dict[str, JsonDict] = {}
         for room_id, room_result in rooms.items():
             serialized_rooms[room_id] = {
-                "bump_stamp": room_result.bump_stamp,
+                "bump_stamp": abs(room_result.bump_stamp),
                 "timestamp": abs(room_result.bump_stamp),
                 "joined_count": room_result.joined_count,
                 "invited_count": room_result.invited_count,
